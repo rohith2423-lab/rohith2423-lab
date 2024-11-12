0d587bac13a8ebd9425dafeecc024ede4d1e523f
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 12:03:40 +0530

    Add files via upload

---
+++ A ~ B.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main () {
+	int a,b,c;
+	
+	printf("Enter a number :");
+	scanf("%d",&a);
+	
+	printf("Enter a number :");
+	scanf("%d",&b);
+	
+	c=~a;
+	printf("%d",c);
+	 // it will take only a(10) it convert the binary in revers form
+	 //10 24
+
+     //16 8 4 2 1
+
+   //0 1 0 1 0
+ 
+   //1 0 1 0 1
+ 
+	
+	
+}
