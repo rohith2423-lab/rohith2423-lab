83cdced7d6755e9df747a44acb09eabb9017136a
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 14:06:17 +0530

    Add files via upload

---
+++ Lift shift.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int a,b,c;
+	printf("Enter a number :");
+	scanf("%d",&a);
+	
+	printf("Enter a thr number of shift :");
+	scanf("%d",&b);
+	b=a>>b;
+	printf("%d",c);	
+}
