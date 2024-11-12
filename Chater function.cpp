cee6d4c36c8d9d388840e87a36d08b42c08ea106
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 12:34:41 +0530

    Add files via upload

---
+++ Chater function.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+char alphbet() ;
+main () {
+	char ch;
+	printf("Enter a charater :");
+	scanf("%c",&ch);
+	alphbet(ch) ;
+}
+char alphbet(ch)
+{
+	
+	( (ch>=a) && (ch<z) || (ch>=A) && (ch<Z) ) ? printf("it is charater"):printf("it is charater");
+ } 
+}
