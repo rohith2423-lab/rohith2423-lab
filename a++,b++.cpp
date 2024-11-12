2bd32193cf80e6e11c18eda526fff7eae40ef1d5
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 12:09:58 +0530

    Add files via upload

---
+++ a++,b++.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+	
+	int a=5;
+	int b=6;
+                                                                      
+	b=++a;
+	a=++b;
+	printf("increament : %d \n",b);
+	printf("decreament : %d \n",a);
+	
+}
