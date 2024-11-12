c33ae5e57551fc46d5e3993f884569cbc48b36d2
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 12:34:58 +0530

    Add files via upload

---
+++ Condition.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+	int a,b;
+	printf("Enter a number :");
+	scanf("%d",&a);
+	
+	printf("Enter a number :");
+	scanf("%d",&b);
+	
+	(a<b)? (printf("%d is greather ",b)) :  (printf("%d is greather ",a) );
+	
+	}
