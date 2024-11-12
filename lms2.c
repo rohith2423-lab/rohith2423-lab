e9625c511efe70060fcdca1b72b5c81b5f3f5334
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 14:06:53 +0530

    Add files via upload

---
+++ lms2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main  () 
+{
+	int a,i;
+	
+	scanf("%d",&a);
+	
+	for(i=1;i<=a;i++)
+	{
+		if(a%2==0)
+		{
+			printf("true");
+		}
+		else if(a%7==0)
+		{
+			printf("false");
+		}
+		else
+		{
+			printf("no");
+		}
+}
+	}
+	
+	
+	
+	
+	
+	
+	
+
