5b875ce67170af11bfe104610ae0d4adccf7463b
rohith2423-lab <2420090147@klh.edu.in>
2024-11-12 14:04:10 +0530

    Add files via upload

---
+++ Greather number.2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main () {
+	int a,b;
+	printf("Enter a number :");
+	scanf("%d %d",&a,&b);
+	
+	if(a>b)
+	printf("a is greather than b");
+	
+	else
+	printf("b is greather than a");
+	
+	
+}
