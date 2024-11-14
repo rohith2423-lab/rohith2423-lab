f7db88ebacf9b02db9ade0259fbd36435bb45fb5
rohith2423-lab <2420090147@klh.edu.in>
2024-11-14 10:22:37 +0530

    Add files via upload

---
+++ Right shift.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+	int a,b,c;
+	printf("Enter a number :");
+	scanf("%d",&a);
+	
+	printf("Enter a thr number of shift :");
+	scanf("%d",&b);
+	c=a>>b;
+	printf("%d",c);
+	  
+	  //it will movr to right side
+	 
+	 //10
+
+    //16 8 4 2 1
+
+     // 1 0 1 0
+      
+     //   1 0 1 0
+
+}
