df081c32b40564855dade74e8c08a2bc590f010e
rohith2423-lab <2420090147@klh.edu.in>
2024-11-14 10:31:46 +0530

    Add files via upload

---
+++ String Strcmp.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main () {
+	int value;
+	char s1[] = "Rohith";
+	char s2[] = "Jenny";
+	
+	value = strcmp(s1,s2);
+	if(value==0)
+	{
+		printf("same");
+	}
+	else
+	{
+		printf("No");
+	}
+	printf("%d",value);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
