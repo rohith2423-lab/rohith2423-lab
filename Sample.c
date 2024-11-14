94bc3f14f200f252a8cd4fe6b5404fbd90852d2f
rohith2423-lab <2420090147@klh.edu.in>
2024-11-14 10:26:08 +0530

    Add files via upload

---
+++ Sample.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main () {
+	int a,b,c;
+	printf("Enter the number of Clovers :");
+	scanf("%d",&a);
+	
+	b=((a-1)*3+4);
+	
+	printf("%d",b);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
