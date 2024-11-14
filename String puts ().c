1077e89eb9fab7ee1993964937871a3d9c8c4515
rohith2423-lab <2420090147@klh.edu.in>
2024-11-14 10:30:29 +0530

    Add files via upload

---
+++ String puts ().c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main () {
+	
+	char name[50];
+	printf("enter a name:");
+	scanf("%s",name);
+	
+//	printf("%.5s",name);  //only 5 letters will print
+
+//   printf("%10.5s",name);  //10 space will take
+	
+	puts(name);
+	puts(name);
+	
+	//it will print full name and in next line
+	
+	
+}
