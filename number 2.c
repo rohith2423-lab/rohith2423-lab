7fa321f7df0339d5ef022121a70428a91d2f1695
rohith2423-lab <2420090147@klh.edu.in>
2024-11-14 10:10:43 +0530

    Add files via upload

---
+++ number 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main () {
+	int a,j,i,b=1;
+	
+	printf("Enter number of rows :");
+	scanf("%d",&a);
+	
+	for(j=1;j<=a;j++)
+	{
+		printf("\n");
+		for(i=1;i<=j;i++)
+		{
+			
+	    printf("%d",b);
+        }
+     	
+     	printf("\n");
+	}
+	
+	
+}
