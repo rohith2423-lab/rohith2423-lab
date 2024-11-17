7639a917e006d62df3a9e4a61b95f8717bd6c67e
rohith2423-lab <2420090147@klh.edu.in>
2024-11-17 15:32:36 +0530

    Add files via upload

---
+++ Arrays Transpose of Matrix.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main () 
+{
+	int a[2][3],i,j;
+	
+	printf("Enter the elements of matrix :");
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	printf("\n");
+	printf("The matrix :");
+	printf("\n");
+	
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\n");
+	printf("Transpose of Martix :");
+	printf("\n");
+	
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",a[j][i]);
+		}
+		printf("\n");
+	}
+	
+}
